c30cc822b65046251a0c1f44a118be2f580c1805
heptoilabas1-bit <heptoilabas1@gmail.com>
2026-09-15 17:18:45 +0200

    Ajoute les comparaisons de l’Avent 2026

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    const DATA_VERSION = '20260915-comparisons-luke-5-9-1';
+    const DATA_VERSION = '20260915-comparisons-advent-2026-1';
     const versionedDataPath = path => `${path}?v=${DATA_VERSION}`;
 
     // --- 1. LISTE DE RÉFÉRENCE DES DIMANCHES ---
